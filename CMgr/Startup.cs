2ecb2bbd7410add687e82d1b5a0a2d699b27b630
stevenlim41 <stevenlim@scopeglobal.com>
2016-08-17 13:05:57 +0930

    Add project files.

---
+++ CMgr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CMgr.Startup))]
+namespace CMgr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
